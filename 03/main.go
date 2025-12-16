efd40c1b9b1fec6a65a1b7cb43ec667e5c2dc719
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-12-15 20:00:01 -0800

    d3p2

--- 03/main.go
+++ 03/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 const Day = "03"
@@ -36,43 +37,41 @@ func Run(debug bool, targetFile string) (int, int) {
 	var part02 int
 
 	for _, line := range lines {
-		part01 += calculateBestJoltage(line)
+		part01 += calculateBestJoltage2(line, 2)
+		part02 += calculateBestJoltage2(line, 12)
 	}
 
-	DEBUGF("Part 1: %d", part01)
+	DEBUGF("Part 1: %d\n", part01)
+	DEBUGF("Part 2: %d", part02)
 
 	return part01, part02
 }
 
-func calculateBestJoltage(line string) int {
-	bestFirstIdx, bestFirstCh := 0, 0
-	bestLastIdx, bestLastCh := len(line)-1, 0
-	for idx, ch := range line {
-		if idx == len(line)-1 {
-			// Last number is off limits
-			break
-		}
-		iCh := int(ch - '0')
-		if iCh > bestFirstCh {
-			bestFirstIdx, bestFirstCh = idx, iCh
-		}
-	}
+func calculateBestJoltage2(line string, numBatteries int) int {
+	length := len(line)
+	lowerBound := 0
+	batteriesAlreadyCollected := 0
+	var result []string = make([]string, 0)
 
-	for idx := len(line) - 1; idx > bestFirstIdx; idx-- {
-		revCh, err := strconv.Atoi(line[idx : idx+1])
-		if err != nil {
-			panic("atoi")
+	for batteriesAlreadyCollected != numBatteries {
+		tmpBestIdx, tmpBestVal, tmpBestString := 0, 0, "X"
+		for idx := lowerBound; idx < (length - (numBatteries - batteriesAlreadyCollected - 1)); idx++ {
+			currValString := line[idx : idx+1]
+			curVal, err := strconv.Atoi(currValString)
+			if err != nil {
+				panic("oops")
+			}
+			if curVal > tmpBestVal {
+				tmpBestIdx, tmpBestVal, tmpBestString = idx, curVal, currValString
+			}
 		}
-		if revCh > bestLastCh {
-			bestLastIdx, bestLastCh = idx, revCh
-		}
+		result = append(result, tmpBestString)
+		batteriesAlreadyCollected++ // We captured a battery
+		lowerBound = tmpBestIdx + 1 // We cannot use any numbers to the left of what we captured
 	}
-
-	concat := fmt.Sprintf("%d%d", bestFirstCh, bestLastCh)
-	DEBUGF("%s  ([%d]-[%d])\n", concat, bestFirstIdx, bestLastIdx)
-	result, err := strconv.Atoi(concat)
+	num, err := strconv.Atoi(strings.Join(result, ""))
 	if err != nil {
-		panic("strconv of concat")
+		panic("reconstruct failure")
 	}
-	return result
+	return num
 }
